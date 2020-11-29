47673ae90c6ee1c3d1b23379b2693bed6a4f1fa6
barrydriscoll <75094052+barrydriscoll@users.noreply.github.com>
2020-11-29 17:27:30 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,4 @@ cd /cpuminer
     git clone http://github.com/bogdanadnan/multiminer
   
     cd multiminer
-    mkdir build
-    cd build
-    cmake ..
-    make
-    chmod 777 multiminer
-    ./multiminer -a yescrypt -o stratum+tcp://koto.multipool.online:30320 -u k1FrpsNoHao9FmggMDdB4dZtYkcNrZNVTUW.m -p x -t 3
+    
